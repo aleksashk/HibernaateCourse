deec14035dab54558166f0b433f08d4c121b93ff
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-23 21:57:17 +0300

    getting java objects from a database with HQL get all employees

---
+++ src/main/java/hibernate_test/Test3.java
@@ -0,0 +1,36 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Timon", "Pumba", "HR", 742);
+            session.beginTransaction();
+
+            List<Employee> emps = session.createQuery("from Employee")
+                            .getResultList();
+            for (Employee e : emps) {
+                System.out.println(e);
+            }
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!!");
+        } finally {
+            factory.close();
+        }
+
+    }
+}
