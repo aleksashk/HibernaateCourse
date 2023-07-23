17dd4c86390f8e3df1aceb90f05f7f503b8d7eb1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-23 21:45:00 +0300

    Initial commit

---
+++ src/main/java/com/aleksandrphilimonov/hibernate_course_2/App.java
@@ -0,0 +1,13 @@
+package com.aleksandrphilimonov.hibernate_course_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
